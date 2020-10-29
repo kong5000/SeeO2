9e297343b6e181c05c590b36cef2c976bc345688
Mark R Baker <mrbaker1917@gmail.com>
2020-10-29 14:07:27 -0700

    changes chart background to white and fixes width

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import Chart from "./Chart";
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import ioClient from 'socket.io-client'
-const socket = ioClient('http://localhost:8002');
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import ioClient from "socket.io-client";
+const socket = ioClient("http://localhost:8002");
 
 //Connect to the backend and render the frontend
-socket.on('connect', ()=>{
-  socket.on('SendSensors', (data)=>{
+socket.on("connect", () => {
+  socket.on("SendSensors", (data) => {
     ReactDOM.render(
       <React.StrictMode>
-        <App 
-          sensors={data}
-          socket={socket}
-        />
+        <App sensors={data} socket={socket} />
       </React.StrictMode>,
-      document.getElementById('root')
+      document.getElementById("root")
     );
   });
 
-  socket.on('receiveHistoricalData', (data)=>{
+  socket.on("receiveHistoricalData", (data) => {
     //Render historical data where the sidebar is
 
     const c02Data = [];
     const tvocData = [];
     //Get data from the last 24 hours
-    for(let i = 0; i < 139; i++){
+    for (let i = 0; i < 139; i++) {
       const dataPoint = {};
-      if(data[i]) {
+      if (data[i]) {
         dataPoint.name = data[i].date;
-        data[i].co2 > 0 ? dataPoint.co2 = data[i].co2 : dataPoint.empty = 100;
+        data[i].co2 > 0
+          ? (dataPoint.co2 = data[i].co2)
+          : (dataPoint.empty = 100);
         c02Data.push(dataPoint);
       } else {
         //Push empty data if none exists
@@ -39,12 +38,14 @@ socket.on('connect', ()=>{
         c02Data.push(dataPoint);
       }
     }
-    for(let i = 0; i < 139; i++){
+    for (let i = 0; i < 139; i++) {
       const dataPoint = {};
-      if(data[i]) {
+      if (data[i]) {
         dataPoint.name = data[i].date;
-        data[i].tvoc > 0 ? dataPoint.tvoc = data[i].tvoc : dataPoint.empty = 100;
-        tvocData.push(dataPoint)
+        data[i].tvoc > 0
+          ? (dataPoint.tvoc = data[i].tvoc)
+          : (dataPoint.empty = 100);
+        tvocData.push(dataPoint);
       } else {
         dataPoint.empty = 100;
         tvocData.push(dataPoint);
@@ -52,20 +53,20 @@ socket.on('connect', ()=>{
     }
 
     ReactDOM.render(
-      <div className="sidebar">
+      <div className="sidebarChart">
         <h1>Last 24 hours:</h1>
-        <Chart data={c02Data} dataKey="co2"/>
-        <Chart data={tvocData} dataKey="tvoc"/>
-      </div>
-      ,
-    document.getElementById('side'));
-  })
+        <Chart data={c02Data} dataKey="co2" />
+        <Chart data={tvocData} dataKey="tvoc" />
+      </div>,
+      document.getElementById("side")
+    );
+  });
 
-  socket.on('alertCreated', (data)=>{
+  socket.on("alertCreated", (data) => {
     alert(data);
-    return
-  })
-})
+    return;
+  });
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
